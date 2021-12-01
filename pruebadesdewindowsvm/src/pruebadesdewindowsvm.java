a0dd208aa2c0edd2e2e8c965745806baae0ae09f
cristinacarrascosa <cctausias@gmail.com>
2021-12-01 13:20:28 +0100

    creado sout hola que tal desde mv

---
+++ pruebadesdewindowsvm/src/pruebadesdewindowsvm.java
@@ -0,0 +1,19 @@
+/*
+ * Proyecto pruebadesdewindowsvm - Archivo pruebadesdewindowsvm.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/**
+ *
+ * @author Cristina Carrascosa <cctausias@gmail.com>
+ * @version 1.0
+ * @date 1 dic. 2021 13:16:44
+ */
+public class pruebadesdewindowsvm {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola que tal?");
+    }
+
+}
